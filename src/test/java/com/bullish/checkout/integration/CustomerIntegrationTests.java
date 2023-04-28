3aa17076cac3d603780636f60e71baff1b7534ec
Varun V <varunv@duck.com>
2023-04-28 16:55:11 +0800

    Add first simple integration test

--- src/test/java/com/bullish/checkout/integration/CustomerIntegrationTests.java
+++ src/test/java/com/bullish/checkout/integration/CustomerIntegrationTests.java
@@ -1,4 +1,31 @@
 package com.bullish.checkout.integration;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CustomerIntegrationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void testAddProductToBasket() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/basket/product"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("addProduct"));
+    }
 }
