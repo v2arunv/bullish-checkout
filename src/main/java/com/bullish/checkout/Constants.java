f9237d469b2b4ef24b24f797fc9febeb52b39724
Varun V <varunv@duck.com>
2023-04-29 15:28:28 +0800

    Add comments

--- src/main/java/com/bullish/checkout/Constants.java
+++ src/main/java/com/bullish/checkout/Constants.java
@@ -1,6 +1,8 @@
 package com.bullish.checkout;
 
 public class Constants {
+
+    // Storing all reused string constants
     public static final String DEAL_BASE_PATH = "/deal";
     public static final String PRODUCT_BASE_PATH = "/product";
     public static final String BASKET_BASE_PATH = "/basket";
--- src/main/java/com/bullish/checkout/configuration/AuthenticationAndSecurityConfiguration.java
+++ src/main/java/com/bullish/checkout/configuration/AuthenticationAndSecurityConfiguration.java
@@ -27,20 +27,25 @@ public class AuthenticationAndSecurityConfiguration {
     private String password;
 
     private final String ADMIN_ROLE = "ADMIN";
-    private String[] protectedPaths = Stream.of(Constants.PRODUCT_BASE_PATH, Constants.DEAL_BASE_PATH)
+    private final String[] protectedPaths = Stream.of(Constants.PRODUCT_BASE_PATH, Constants.DEAL_BASE_PATH)
             .map(x -> x+"/**").toArray(String[]::new);
 
-    private String[] unprotectedPaths = Stream.of(Constants.BASKET_BASE_PATH)
+    private final String[] unprotectedPaths = Stream.of(Constants.BASKET_BASE_PATH)
             .map(x -> x+"/**").toArray(String[]::new);
 
 
     @Bean
     @Order(2)
     public SecurityFilterChain allSecurity(HttpSecurity http) throws Exception {
+        // NOT OK FOR PROD. But for the purpose of the assignment, it'll do
         http.cors().and().csrf().disable();
+
         http.authorizeHttpRequests(authorizer -> {
             try {
+                // Allow all calls to /basket to go through without security
                 authorizer.requestMatchers(unprotectedPaths).permitAll().and().anonymous();
+
+                // All calls to other endpoints require admin credentials
                 authorizer.requestMatchers(protectedPaths).authenticated()
                         .and()
                         .httpBasic(Customizer.withDefaults());
